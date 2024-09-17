c24187b6955be856eeabe067cc8bfe39fa468042
tirmex-rs <redessocialestirmex@gmail.com>
2024-09-17 17:22:54 -0500

    Add files via upload

---
+++ COTIZACIONES github/resources/js/script.js
@@ -0,0 +1,33 @@
+const toggler = document.querySelector(".btn");
+toggler.addEventListener("click",function(){
+    document.querySelector("#sidebar").classList.toggle("collapsed");
+});
+
+
+//Modal de creacion de nuevas cotizaciones 
+
+// Ventana modal
+var modal = document.getElementById("ventanaModal");
+
+// Botón que abre el modal
+var boton = document.getElementById("abrirModal");
+
+// Hace referencia al elemento <span> que tiene la X que cierra la ventana
+var span = document.getElementsByClassName("cerrar")[0];
+
+// Cuando el usuario hace click en el botón, se abre la ventana
+boton.addEventListener("click",function() {
+  modal.style.display = "flex";
+});
+
+// Si el usuario hace click en la x, la ventana se cierra
+span.addEventListener("click",function() {
+  modal.style.display = "none";
+});
+
+// Si el usuario hace click fuera de la ventana, se cierra.
+window.addEventListener("click",function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
